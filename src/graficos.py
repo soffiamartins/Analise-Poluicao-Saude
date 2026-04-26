a6f10252df72497f0fa54c23a173cffe44782f67
Soffia <soffiadejesus@gmail.com>
2026-04-26 18:03:04 -0300

    pasta iniciada

---
+++ src/graficos.py
@@ -0,0 +1,4 @@
+#gráfico de linha: usar a métrica internações por mês;
+#gráfico de linhas múltiplas: usar a métrica poluentes por mês;
+#gráfico de dispersão: usar a métrica poluente x internações;
+#gráfico de barras horizontais: usar a métrica ranking de municípios.
